be4734ca3b7fb5c5e91b523cea72100da3e29166
David Herman <bitspittle@gmail.com>
2024-02-20 17:36:10 -0800

    Update the handleConf method
    The new version no longer uses reflection but rather just
    parses Yaml in a generic way, making it more flexible.
    
    It also includes a list of possible queries on failure.

--- kobweb/src/main/kotlin/com/varabyte/kobweb/cli/conf/Conf.kt
+++ kobweb/src/main/kotlin/com/varabyte/kobweb/cli/conf/Conf.kt
@@ -1,35 +1,61 @@
 package com.varabyte.kobweb.cli.conf
 
+import com.charleskorn.kaml.Yaml
+import com.charleskorn.kaml.YamlMap
+import com.charleskorn.kaml.YamlNode
+import com.charleskorn.kaml.YamlScalar
+import com.charleskorn.kaml.yamlMap
 import com.varabyte.kobweb.cli.common.assertKobwebApplication
 import com.varabyte.kobweb.cli.common.assertKobwebConfIn
+import com.varabyte.kobweb.project.conf.KobwebConfFile
 import java.io.File
+import kotlin.io.path.readText
+
+private fun YamlMap.collectValuesInto(map: MutableMap<String, String>, pathPrefix: List<String>) {
+    this.entries.forEach { (key, value) ->
+        if (value is YamlScalar) {
+            val keyPrefix = if (pathPrefix.isNotEmpty()) pathPrefix.joinToString(".") + "." else ""
+            map["$keyPrefix${key.content}"] = value.content
+        } else if (value is YamlMap) {
+            value.collectValuesInto(map, pathPrefix + key.content)
+        }
+    }
+}
+
+private fun YamlNode.collectValues(): Map<String, String> {
+    val scalars = mutableMapOf<String, String>()
+    yamlMap.collectValuesInto(scalars, emptyList())
+    return scalars
+}
 
 fun handleConf(query: String, projectDir: File) {
     val kobwebApplication = assertKobwebApplication(projectDir.toPath())
     if (query.isBlank()) return // No query? OK I guess we're done
 
-    val conf = assertKobwebConfIn(kobwebApplication.kobwebFolder)
+    // Even though we don't use its return value, we use this as a side effect to show a useful error message to users
+    // if the conf.yaml file is not found.
+    assertKobwebConfIn(kobwebApplication.kobwebFolder)
 
-    // Use reflection to convert a query, e.g. "server.port", into an answer
-    // We basically loop through all properties in the parsed conf.yaml and, if it matches the current part of the
-    // query (e.g. "server"), we invoke the getter and continue this until we get to the terminating query part
-    // (e.g. "port"), at which point the get value is the answer.
-    var answer: Any? = null
-    query.split(".").toMutableList().let { queryParts ->
-        var ptr: Any = conf
-        while (queryParts.isNotEmpty()) {
-            // Use Java reflection instead of Kotlin reflection to avoid a 2MB dependency size penalty
-            val nextPart = "get" + queryParts.removeFirst().capitalize() // e.g. server -> getServer
-            val matchingMethod = ptr::class.java.methods.singleOrNull { it.name == nextPart } ?: break
-            ptr = matchingMethod.invoke(ptr)
-            if (queryParts.isEmpty()) {
-                answer = ptr
-            }
-        }
-    }
+    // Instead of using Kobweb's KobwebConf deserialized class, we use a generically parsed Yaml result. This ensures
+    // that this command will work flexibly with any version of any conf.yaml file, even if a new field has since been
+    // added.
+    val yamlNode = Yaml.default.parseToYamlNode(KobwebConfFile(kobwebApplication.kobwebFolder).path.readText())
+
+    val yamlValues = yamlNode.collectValues()
+    val answer = yamlValues[query]
+
     if (answer != null) {
         println(answer)
     } else {
-        System.err.println("Invalid query")
+        System.err.println(buildString {
+            appendLine("Invalid query.")
+            appendLine()
+            appendLine("Possible queries are:")
+
+            yamlValues.keys.sorted().forEach { key ->
+                append(" • ")
+                appendLine(key)
+            }
+        })
     }
 }
