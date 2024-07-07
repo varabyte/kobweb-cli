02362c35630f0c985b2aab5d42e90026f1852d5b
Ellet <ellet@freshplatform.net>
2024-07-07 17:49:14 +0300

    chore: migrate to clikt 4.4.0 with mordant

--- kobweb/src/main/kotlin/main.kt
+++ kobweb/src/main/kotlin/main.kt
@@ -4,7 +4,7 @@ import com.github.ajalt.clikt.core.ParameterHolder
 import com.github.ajalt.clikt.core.UsageError
 import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.output.CliktHelpFormatter
+import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.options.convert
@@ -164,7 +164,12 @@ fun main(args: Array<String>) {
     class Kobweb : NoOpCliktCommand() {
         init {
             context {
-                helpFormatter = CliktHelpFormatter(showDefaultValues = true)
+                helpFormatter = { context ->
+                    MordantHelpFormatter(
+                        context = context,
+                        showDefaultValues = true,
+                    )
+                }
                 helpOptionNames += "help" // Allows "kobweb help" to work
             }
         }
