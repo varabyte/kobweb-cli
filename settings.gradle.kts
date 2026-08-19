9dc1230ce2ff7155392d230e58730676ccab4450
David Herman <bitspittle@gmail.com>
2026-08-19 12:53:31 -0700

    Clean up kobweb-cli-compilation-settings
    ... moving repository management to the settings.gradle.kts file and
    also using the simpler "options.release" value for setting the JVM
    target.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,40 @@
+
+
+import org.gradle.kotlin.dsl.repositories
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
     }
     includeBuild("gradle/build-logic")
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+
+        // Special Gradle repo for gradle tooling library
+        exclusiveContent {
+            forRepository {
+                maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+            }
+            filter {
+                includeGroup("org.gradle")
+            }
+        }
+
+        maven {
+            // For kotter snapshots and Kobweb artifacts
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+            mavenContent {
+                includeGroup("com.varabyte.kotter")
+                includeGroupByRegex("com\\.varabyte\\.kobwebx?")
+                snapshotsOnly()
+            }
+        }
+    }
 }
 
 rootProject.name = "kobweb-cli"
